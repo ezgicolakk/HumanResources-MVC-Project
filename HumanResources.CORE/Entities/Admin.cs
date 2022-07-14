986bf63068536f276e96fa164e8f5040d27c06a4
Ezgi Çolak <ezgi_colaq@hotmail.com>
2022-07-14 11:46:19 +0300

    Add project files.

---
+++ HumanResources.CORE/Entities/Admin.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResources.CORE.Entities
+{
+    public class Admin:BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
+        public DateTime BirthDate { get; set; }
+        public string Country { get; set; }
+        public string ProfilePictureName { get; set; }
+        [NotMapped]
+        public IFormFile ProfilePictureFile { get; set; }
+    }
+}
