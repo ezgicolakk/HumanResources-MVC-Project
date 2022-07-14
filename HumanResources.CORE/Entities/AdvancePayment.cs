aa11f96d2b6ae25701eaebaa9b6f54f663c9c530
Ezgi Çolak <ezgi_colaq@hotmail.com>
2022-07-14 22:15:37 +0300

    Advance Payment done.

--- HumanResources.CORE/Entities/AdvancePayment.cs
+++ HumanResources.CORE/Entities/AdvancePayment.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HumanResources.CORE.Entities
 {
-    internal class AdvancePayment
+    public class AdvancePayment:BaseEntity
     {
+        [Display(Name ="Toplam Ödeme")]
+        public decimal TotalPaymentRequest { get; set; }
+
+        [Display(Name ="Açıklama")]
+        public string Description { get; set; }
+
+        public DateTime CreateTime { get; set; } = DateTime.Now;
+        public string FileName { get; set; }
+        [NotMapped]
+        public IFormFile File{ get; set; }
+
     }
 }
