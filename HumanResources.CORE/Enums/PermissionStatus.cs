523431825bdd2007a8e7de5213da35fad12f231b
Ezgi Çolak <ezgi_colaq@hotmail.com>
2022-07-15 08:52:01 +0300

    Enums(PermissionStatus) created.

---
+++ HumanResources.CORE/Enums/PermissionStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResources.CORE.Enums
+{
+    public enum PermissionStatus
+    {
+        Onaylandi,
+        Reddedildi,
+        Bekliyor
+    }
+}
